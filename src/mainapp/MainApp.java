0427c5b38fe475bebff347ee3524b23dc9e732bc
Daniel Serrano Rodríguez <a_daniel.serrano.rodriguez@iespablopicasso.es>
2021-11-19 19:02:21 +0100

    Se ha realizado una version completamente funcional. Necesita retoques para la comprobacion de errores total

--- src/mainapp/MainApp.java
+++ src/mainapp/MainApp.java
@@ -9,8 +9,8 @@ import models.Persona;
 public class MainApp {
 	static Scanner sc = new Scanner(System.in);
 	static String menu1;
-	static int menu2 = 0;
-	static int cont = 1;
+	static int menu2;
+	static int cont = 0;
 	static Persona per1 = new Persona("Dani", "Serra", "12345678Z", 2700);
 	static Persona per2 = new Persona("Jose", "Lopez", "98765432A", 1000);
 	static Persona per3 = new Persona("Pedro", "Martinez", "01928374G", 2000);
@@ -18,28 +18,9 @@ public class MainApp {
 	static CuentaCorriente cc2 = new CuentaCorriente(5678, 0, per2);
 	static CuentaCorriente cc3 = new CuentaCorriente(9012, 0, per3);
 	static ArrayList<Persona> gente = new ArrayList<Persona>();
-	static String[] array = new String[0];
-	static int count = gente.size();
+	static int contador = 1;
 
 	public static void main(String[] args) {
-
-//		Persona per1 = new Persona("Dani", "Serra", "12345678Z", 2700, cc1);
-//		Persona per2 = new Persona("Jose", "Lopez", "98765432A", 1000, cc2);
-//		Persona per3 = new Persona("Pedro", "Martinez", "01928374G", 2000, cc3);
-
-//		
-//		per1.cobrarSueldo();
-//		per1.sacarPasta(300);
-//		try {
-//			per1.subirSueldo(3000);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-
-//		System.out.println(per1 + "\n" + per2 + "\n" + per3);
-//		System.out.println(cc1 + "\n" + cc2 + "\n" + cc3);
-//		System.out.println(per1);
-//		System.out.println(cc);
 
 		do {
 			System.out.println("a. Anadir persona");
@@ -57,7 +38,7 @@ public class MainApp {
 	private static void opciones() {
 		switch (menu1) {
 		case "a":
-			System.out.println("Introduce los datos de la persona\n");
+			System.out.println("\nIntroduce los datos de la persona");
 			System.out.print("Nombre: ");
 			String nom = sc.next();
 			System.out.print("Apellido: ");
@@ -66,9 +47,51 @@ public class MainApp {
 			String dni = sc.next();
 			System.out.print("Sueldo: ");
 			Double sueldo = Double.parseDouble(sc.next());
+			Persona persona = new Persona(nom, ape, dni, sueldo);
+			CuentaCorriente cuenta = new CuentaCorriente(contador, 0, persona);
+			persona.setCuenta(cuenta);
+			gente.add(persona);
+			System.out.println();
+			break;
 
-			Persona arr = new Persona(nom, ape, dni, sueldo);
-			gente.add(arr);
+		case "b":
+			System.out.println();
+			if (gente.size() > 0) {
+				int cont1 = 1;
+				for (Persona pers : gente) {
+					System.out.println(cont1++ + ". " + pers.getDni() + " " + pers.getNombre());
+				}
+				System.out.print("Elige a la persona: ");
+				menu2 = Integer.parseInt(sc.next());
+				cont1 = 1;
+				try {
+					menuPersona(gente.get(menu2 - 1));
+				} catch (Exception e) {
+					System.out.println("Elige un numero válido");
+				}
+			} else {
+				System.out.println("No hay gente");
+			}
+			System.out.println();
+			break;
+
+		case "c":
+			int cont2 = 1;
+			System.out.println();
+			for (Persona pers : gente) {
+				System.out.println(cont2++ + ". " + pers.getDni() + " " + pers.getNombre());
+			}
+			System.out.print("¿Que cuenta quieres borrar? ");
+			menu2 = Integer.parseInt(sc.next());
+			gente.remove(menu2 - 1);
+			cont2 = 1;
+			System.out.println();
+			break;
+
+		case "d":
+			System.out.println();
+			menu1 = "d";
+			System.out.println("Ta luego figura");
 			break;
 
 		default:
@@ -76,47 +99,60 @@ public class MainApp {
 		}
 	}
 
-//	private static void menuPersona() {
-//		do {
-//			System.out.println("1. Cobrar sueldo");
-//			System.out.println("2. Sacar pasta");
-//			System.out.println("3. Subir sueldo");
-//			System.out.println("4. Mostrar detalles de la persona");
-//			System.out.println("5. Salir");
-//			System.out.print(": ");
-//			menu2 = Integer.parseInt(sc.next());
-//			accionesPersona();
-//		} while (menu2 != -2);
-//	}
-//
-//	private static void accionesPersona() {
-//		switch (menu2) {
-//		case 1:
-//			per1.cobrarSueldo();
-//			break;
-//
-//		case 2:
-//			System.out.println("¿Cuanta pasta? ");
-//			per1.sacarPasta(Integer.parseInt(sc.next()));
-//			break;
-//
-//		case 3:
-//			System.out.println("¿Cuanto quieres subirlo? ");
-//			per1.sacarPasta(Integer.parseInt(sc.next()));
-//			break;
-//
-//		case 4:
-//			System.out.println(per1);
-//			break;
-//
-//		case 5:
-//			menu2 = -2;
-//			break;
-//
-//		default:
-//			System.out.println("Introduce una opcion correcta");
-//			break;
-//		}
-//	}
+	private static void menuPersona(Persona persona) {
+		do {
+			System.out.println("1. Cobrar sueldo");
+			System.out.println("2. Sacar pasta");
+			System.out.println("3. Subir sueldo");
+			System.out.println("4. Mostrar detalles de la persona");
+			System.out.println("5. Salir");
+			System.out.print(": ");
+			menu2 = Integer.parseInt(sc.next());
+			accionesPersona(persona);
+		} while (menu2 != -2);
+	}
+
+	private static void accionesPersona(Persona persona) {
+		switch (menu2) {
+		case 1:
+			persona.cobrarSueldo();
+			System.out.println();
+			break;
+
+		case 2:
+			System.out.println();
+			System.out.println("¿Cuanta pasta? ");
+			persona.sacarPasta(Integer.parseInt(sc.next()));
+			System.out.println();
+			break;
+
+		case 3:
+			System.out.println();
+			System.out.println("¿Cuanto quieres subirlo? ");
+			try {
+				persona.subirSueldo(Double.parseDouble(sc.next()));
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println();
+			break;
+
+		case 4:
+			System.out.println(persona);
+			System.out.println();
+			break;
+
+		case 5:
+			menu2 = -2;
+			System.out.println();
+			break;
+
+		default:
+			System.out.println("Introduce una opcion correcta");
+			break;
+		}
+	}
 
 }
